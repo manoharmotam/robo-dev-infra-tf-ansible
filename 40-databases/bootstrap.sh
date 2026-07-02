05ff27e0178637cc2d5393b929b193de393943bc
manoharmotam <b3stsinceday1@gmail.com>
2026-07-02 09:58:42 +0000

    Add: database config files

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -7,10 +7,10 @@ dnf install ansible -y
 mkdir -p /var/log/ansible
 chown -R ec2-user:ec2-user
 chmod -R 755 /var/log/mrmotam
-touch /var/log/mrmotam/$COMPONENT.log
+touch /var/log/mrmotam/"$COMPONENT".log
 
 cd /home/ec2-user
 git clone https://github.com/manoharmotam/ansible-dev-tf-integration.git
 cd ansible-dev-tf-integration
 git pull 2> /dev/null
-ansible-playbook -e COMPONENT=$COMPONENT -e ENVIRONMENT=$ENVIRONMENT robo.yml
+ansible-playbook -e COMPONENT="$COMPONENT" -e ENVIRONMENT="$ENVIRONMENT" robo.yml
