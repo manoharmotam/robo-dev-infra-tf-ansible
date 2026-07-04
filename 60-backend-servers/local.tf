d1854fe7ab289ad0028d657032db87bcebb3fe8d
manoharmotam <b3stsinceday1@gmail.com>
2026-07-04 17:16:51 +0000

    Add: backend servers config

---
+++ 60-backend-servers/local.tf
@@ -0,0 +1,20 @@
+locals {
+  common_name = "${var.project}-${var.environment}"
+  common_tags = {
+    Project      = "${var.project}"
+    Environment  = "${var.environment}"
+    Name         = "${local.common_name}"
+    "Managed by" = "Terraform"
+  }
+  ami_id         = data.aws_ami.ami_id.id
+  instance_type  = var.instance_type
+  catalogue_sg_id  = data.aws_ssm_parameter.catalogue_sg_id.value
+  user_sg_id    = data.aws_ssm_parameter.user_sg_id.value
+  cart_sg_id    = data.aws_ssm_parameter.cart_sg_id.value
+  shipping_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
+  payment_sg_id = data.aws_ssm_parameter.payment_sg_id.value
+  subnet_id      = split(",", data.aws_ssm_parameter.subnet_ids.value)[0]
+  iam_instance_profile = aws_iam_instance_profile.mysql.name
+
+  domain_name = "${var.project}.online"
+}
---
+++ 60-backend-servers/variable.tf
@@ -0,0 +1,23 @@
+variable "project" {
+  default = "mrmotam"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+variable "ec2_tags" {
+  default = {}
+}
+
+variable "mysql_password" {
+  type = string
+}
+
+variable "zone_id" {
+  default = "Z00263282318BT9FBW1XK"
+}
+
