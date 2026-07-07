85f9c48b0ffc26bbcbbc4a1d5095af3398024336
manoharmotam <b3stsinceday1@gmail.com>
2026-07-07 15:07:58 +0000

    Update: config files for testing

--- 60-backend-servers/r53.tf
+++ 60-backend-servers/r53.tf
@@ -7,38 +7,38 @@ resource "aws_route53_record" "catalogue" {
   allow_overwrite = true
 }
 
-resource "aws_route53_record" "user" {
-  zone_id         = var.zone_id
-  name            = "user-${var.environment}.${local.domain_name}"
-  type            = "A"
-  ttl             = 1
-  records         = [aws_instance.user.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "user" {
+#   zone_id         = var.zone_id
+#   name            = "user-${var.environment}.${local.domain_name}"
+#   type            = "A"
+#   ttl             = 1
+#   records         = [aws_instance.user.private_ip]
+#   allow_overwrite = true
+# }
 
-resource "aws_route53_record" "cart" {
-  zone_id         = var.zone_id
-  name            = "cart-${var.environment}.${local.domain_name}"
-  type            = "A"
-  ttl             = 1
-  records         = [aws_instance.cart.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "cart" {
+#   zone_id         = var.zone_id
+#   name            = "cart-${var.environment}.${local.domain_name}"
+#   type            = "A"
+#   ttl             = 1
+#   records         = [aws_instance.cart.private_ip]
+#   allow_overwrite = true
+# }
 
-resource "aws_route53_record" "shipping" {
-  zone_id         = var.zone_id
-  name            = "shipping-${var.environment}.${local.domain_name}"
-  type            = "A"
-  ttl             = 1
-  records         = [aws_instance.shipping.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "shipping" {
+#   zone_id         = var.zone_id
+#   name            = "shipping-${var.environment}.${local.domain_name}"
+#   type            = "A"
+#   ttl             = 1
+#   records         = [aws_instance.shipping.private_ip]
+#   allow_overwrite = true
+# }
 
-resource "aws_route53_record" "payment" {
-  zone_id         = var.zone_id
-  name            = "payment-${var.environment}.${local.domain_name}"
-  type            = "A"
-  ttl             = 1
-  records         = [aws_instance.payment.private_ip]
-  allow_overwrite = true
-}
+# resource "aws_route53_record" "payment" {
+#   zone_id         = var.zone_id
+#   name            = "payment-${var.environment}.${local.domain_name}"
+#   type            = "A"
+#   ttl             = 1
+#   records         = [aws_instance.payment.private_ip]
+#   allow_overwrite = true
+# }
