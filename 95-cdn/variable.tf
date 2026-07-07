767b570fd8c19288da401d3c774780ef5e90f769
manoharmotam <b3stsinceday1@gmail.com>
2026-07-07 19:15:43 +0000

    Add: 95-CDN config file

---
+++ 95-cdn/variable.tf
@@ -0,0 +1,15 @@
+variable "project" {
+  default = "mrmotam"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z07832896OMO79UC2DE5"
+}
+
+variable "app_version" {
+  default = "v3"
+}
